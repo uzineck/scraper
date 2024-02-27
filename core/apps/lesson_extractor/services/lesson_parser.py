acef624f8989b5532882dfe54b91f88d4e861ebc
SaiGerkat <illyakrykalov@gmail.com>
2024-02-27 22:59:41 +0200

    first text parser functionality

--- core/apps/lesson_extractor/services/lesson_parser.py
+++ core/apps/lesson_extractor/services/lesson_parser.py
@@ -1,5 +1,56 @@
+import re
+
+from dataclasses import dataclass, field
 from abc import ABC, abstractmethod
 
+from core.common.constants import CHECK_VALUES
 
+
+@dataclass
 class BaseLessonParser(ABC):
-    ...
+    teacher_ranks: CHECK_VALUES.TEACHERS_RANKS = field(default=CHECK_VALUES.TEACHERS_RANKS.value)
+
+    @abstractmethod
+    def parse_string(self, string: str):
+        ...
+
+
+class LessonParserService(BaseLessonParser):
+    def parse_string(self, string: str):
+        if "/" not in string and "+" not in string:
+            lesson_teacher_dict = self.parse_string_without_additions(string=string)
+            return lesson_teacher_dict
+        if "/" in string:
+            return self.parse_slash_string(string=string)
+        if "+" in string:
+            return self.parse_plus_string(string=string)
+
+    def parse_string_without_additions(self, string: str):
+
+        rank_pattern = '|'.join(re.escape(rank) for rank in self.teacher_ranks)
+        matches = re.finditer(rank_pattern, string)
+
+        indexes = [match.start() for match in matches]
+
+        divider = min(indexes) if indexes else len(string)
+
+        lesson = string[:divider].strip(" ")
+        teacher = string[divider:].strip(" ")
+
+        if "," in teacher:
+            teachers = re.split(r',(?![^(]*\))', teacher)
+            return {"Lesson": lesson,
+                    "Teacher": [teacher.strip() for teacher in teachers]}
+        return {"Lesson": lesson,
+                "Teacher": teacher}
+
+    def parse_slash_string(self, string: str):
+        string_list = string.split('/')
+
+        lessons = []
+        teachers = []
+
+        for string in string_list:
+            lesson_teacher_dict = self.parse_string_without_additions(string=string)
+
+
--- core/starter/main.py
+++ core/starter/main.py
@@ -7,6 +7,7 @@ from core.apps.groups.services.groups import GroupsService
 from core.apps.daysNnumbers.services.days import DaysService
 from core.apps.daysNnumbers.services.classes_count import ClassesCountService
 from core.apps.lesson_extractor.services.lesson_extractor import LessonExtractorService
+from core.apps.lesson_extractor.services.lesson_parser import LessonParserService
 
 
 def main(sheet: Worksheet, sheet_name: str):
@@ -16,13 +17,14 @@ def main(sheet: Worksheet, sheet_name: str):
     # classes_count = (ClassesCountService(sheet=sheet, sheet_name=sheet_name)
     #                 .get_number_of_classes_for_sheet(days_coordinates=days_coords))
 
-    lesson = LessonExtractorService(sheet=sheet).get_lesson(sheet=sheet, column=column, row=row)
+    lesson = LessonParserService().parse_string(string="Друга іноземна мова"
+                                                       "ст. викл. Пітерова Н.А.   , викл. Гаврилюк Д.М., доц. Вакуленко "
+                                                       "Т.І., доц. Мозолевська А.С.")
 
     return lesson
 
 
 if __name__ == "__main__":
-
     # for sheet_name in wb.sheetnames:
     #     sheet = wb[sheet_name]
     #     print(f'{sheet_name}: ', main(sheet=sheet, sheet_name=sheet_name))
